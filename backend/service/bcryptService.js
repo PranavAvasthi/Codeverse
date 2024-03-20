f4c00fba41445930835bca73efb807aee35a7996
Pranav Avasthi <96632158+PranavAvasthi@users.noreply.github.com>
2024-03-20 12:49:14 +0530

    Create bcryptService.js

---
+++ backend/service/bcryptService.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+var rounds = 10;
+async function hashPass(pass){
+    let  salt =  await bcrypt.genSalt(rounds);
+    let hash = await bcrypt.hash(pass,salt); 
+    return hash;
+}
+
+async function comparePass(pass, hash){
+    let ans = await bcrypt.compare(pass, hash);
+    return ans;
+}
+
+async function test(userpass, newpass){
+    let h1 = await hashPass(userpass);
+    console.log(h1);
+    let ans3 = await bcrypt.compare(newpass, h1);
+    console.log(ans3);
+}
+test('shopclue', 'shopclue');
+
+module.exports = {hashPass, comparePass}
